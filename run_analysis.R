4551b01bbeacd226558b2be8bee435dfc4153f68
csmithbrian <csmithbrian@gmail.com>
2015-07-24 16:00:35 -0400

    updating

--- run_analysis.R
+++ run_analysis.R
@@ -6,21 +6,21 @@ if(length(new.packages)) install.packages(new.packages)
 require(dplyr)
 
 if(!file.exists("./data")){dir.create("./data")}
-if(!file.exists("UCI HAR Dataset")){
+if(!file.exists("./data/UCI HAR Dataset")){
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 print("Downloading file...")
 download.file(fileUrl,destfile="./data/getdata-projectfiles-UCI HAR Dataset.zip")
 print("Download finished. Unzipping...")
-unzip("./data/getdata-projectfiles-UCI HAR Dataset.zip")
+unzip("./data/getdata-projectfiles-UCI HAR Dataset.zip",exdir = "./data/UCI HAR Dataset")
 print("Unzip finished.")}
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
-features <- read.table("UCI HAR Dataset/features.txt")
-activity_names <- read.table("UCI HAR Dataset/activity_labels.txt")
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+features <- read.table("./data/UCI HAR Dataset/features.txt")
+activity_names <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 
 #apply variable names from features.text to x_train & x_test
 for (i in 1:561){
